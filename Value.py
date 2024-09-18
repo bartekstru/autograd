36e6c11d25a3d2759362e26f7ed9112ccc5fa4f8
bartekstru <bartek.stru@gmail.com>
2024-09-18 21:08:58 +0200

    Initial commit

---
+++ Value.py
@@ -0,0 +1,104 @@
+class Value:
+
+    def __init__(self, data, _children=(), _op='', _label=''):
+        self._prev = set(_children)
+        self._op = _op
+        self._label = _label
+        self._backward = lambda: None
+        self.data = data
+        self.grad = 0.0
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+    def __add__(self, other: "Value"):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            # our job is to propagate the gradient of out with respect to all the inputs to the previous nodes
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __radd__(self, other): #other + self
+        return self + other
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return other + (-self)
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def __rmul__(self, other): #other * self
+        return self * other
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only support int/float powers"
+        out = Value(self.data**other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other-1)) * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def __rpow__(self, other):
+        return Value(other**self.data, (self, ), f'**{other}')
+
+    def __truediv__(self, other): #self/other
+        return self * other**-1
+
+    def __rtruediv__(self, other): #other/self
+        return other * self**-1
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
+        out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad
+            
+        out._backward = _backward
+        return out
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
